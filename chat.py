142b13c93ad84781abbec319aad904d08cfea7e8
YourName <your-email@gmail.com>
2026-07-01 18:10:26 +0530

    update

--- chat.py
+++ chat.py
@@ -8,7 +8,7 @@ from rag import search_catalog
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 OFF_TOPIC = [
     "weather",
